34be989bad925f21fa80714bf76dc4f8255c406a
subrataroy321 <subrata.r321@gmail.com>
2020-09-28 19:48:51 -0400

    setup socket server for creating a quiz

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const cors = require("cors")
 const port = process.env.PORT || 8000
 const passport = require("passport")
 const users = require('./routes/api/users')
+const http = require("http");
+const socketIo = require("socket.io");
 
 // middleware
 app.use(cors())
@@ -17,10 +19,49 @@ app.use(passport.initialize())
 // importing passport file into server
 require("./config/passport")(passport)
 
+const server = http.createServer(app);
+const io = socketIo(server);
+
+var MongoClient = require('mongodb').MongoClient;
+var MONGO_URI = process.env.MONGO_URI;
+
+io.on("connection", (socket) => {
+
+  console.log("New client connected");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+
+  socket.on('newQuiz', function(data){
+    MongoClient.connect(MONGO_URI, function(err, db){
+        if (err) throw err;
+        var dbo = db.db('quizzy');
+        dbo.collection('quizzyGames').find({}).toArray(function(err, result){
+            if(err) throw err;
+            var num = Object.keys(result).length;
+            if(num == 0){
+              data.id = 1
+              num = 1
+            }else{
+              data.id = result[num -1 ].id + 1;
+            }
+            var game = data;
+            dbo.collection("quizzyGames").insertOne(game, function(err, res) {
+                if (err) throw err;
+                db.close();
+            });
+            db.close();
+            socket.emit('startGameFromCreator', num);
+        });
+        
+    });
+
+  });
+});
+
 // <<<<<<<<  SERVER HOME ROUTE >>>>>>>>>>>
 app.get("/", (req, res) => {
-  res
-    .status(200)
+  res.status(200)
     .json({ message: "Smile, You are being watched by the Backend Team" })
 })
 
@@ -28,6 +69,6 @@ app.get("/", (req, res) => {
 app.use('/api/users', users)
 
 // SERVER PORT TO LISTEN
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`You are lisening smooth to port ${port}`)
 })
