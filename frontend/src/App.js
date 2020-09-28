9711e488e7be6b4648a38b51e166ddb965ab0184
subrataroy321 <subrata.r321@gmail.com>
2020-09-28 01:35:06 -0400

    [frontend] initialized react app for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h2>Welcome to Quizzy</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
