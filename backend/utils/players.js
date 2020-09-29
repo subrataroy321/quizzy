cd7e6d2c9c4e73a5ebc4633701c293786e26f09a
subrataroy321 <subrata.r321@gmail.com>
2020-09-29 12:33:54 -0400

    [utils] add player constructor class

---
+++ backend/utils/players.js
@@ -0,0 +1,33 @@
+class Players {
+    constructor() {
+        this.Players = []
+    }
+
+    addPlayer(hostId, playerId, name, gameData){
+        var player = {hostId, playerId, name, gameData}
+        this.players.push(player)
+        return player
+    }
+
+    removePlayer(playerId){
+        var player = this.getPlayer(playerId)
+
+        if(player){
+            this.players = this.players.filter((player) => player.playerId !== playerId)
+        }
+        return player
+    }
+
+    getPlayer(playerId){
+        var player = this.players.filter((player) => player.playerId === playerId)
+        return player[0]
+    }
+
+    getPlayers(hostId){
+        var player = this.players.filter((player) => player.hostId === hostId)
+        return player[0]
+    }
+
+}
+
+module.exports = {Players}
