c8dcbb0d13f1a93b7a74d598e46e3548aa9f2e25
subrataroy321 <subrata.r321@gmail.com>
2020-10-05 01:38:20 -0400

    [About] add Data to about page and styling, add responsive styling to all needed elements

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,18 +5,18 @@ import linkedInLogo from '../assets/images/linkedin-in-logo-png-1.png'
 
 const Footer = () => {
   return (
-    <footer className="footer bg-dark">
-        <span className="text-muted">
+    <footer className="footer">
+      <div className="bg-dark">
+        <div className="text-muted">
           <a href="https://github.com/subrataroy321" target="_blank" rel="noopener">
             <img src={gitHubLogo} alt="github" className="social1"/>
           </a>
-        </span>
-        <span className="text-muted">Build and Developed by Subrata Roy</span>
-        <span className="text-muted">
           <a href="https://www.linkedin.com/in/subrataroy321/" target="_blank" rel="noopener">
             <img src={linkedInLogo} alt="linkedin" className="social2"/>
           </a>
-        </span>
+        </div>
+        <span className="text-muted">Build and Developed by Subrata Roy</span>
+      </div>
     </footer>
   )
 }
